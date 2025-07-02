3d2d7df9f18461cc4dc42fc37eb4b42f6fee6ab9
steveme6 <steveme6@admin.com>
2025-07-02 11:32:09 +0800

    add faiss_embedding.py,support faiss

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
+from app.faiss_embedding import FaissEmbeddings
 from app.origin_agent import OriginAgent
 from app.final_agent import FinalAgent
 __all__ = [
     "OriginAgent",
     "FinalAgent",
+    "FaissEmbeddings",
 ]
